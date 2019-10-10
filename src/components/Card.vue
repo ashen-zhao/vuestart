a79327d2c3d0d9d088af5ad3a9dc01ffcfc3041d
zhaoashen <zhaoashen@gmail.com>
2019-10-10 14:47:35 +0800

    first

---
+++ src/components/Card.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="content">
+    <div class="top-line">
+      <div class="blue"></div>
+      <div class="red"></div>
+    </div>
+    <div class="company">
+      <div class="name">
+        <i>{{infos.companyName}}</i>
+      </div>
+      <div class="name-en">
+        <i>{{infos.companyNameEn}}</i>
+      </div>
+    </div>
+    <div class="person">
+      <div class="person-name">
+        <lable class="name">{{infos.personName}}</lable>
+        <label class="work">{{infos.work}}</label>
+      </div>
+      <div class="person-address">地址：{{infos.address}}</div>
+      <div class="person-phone">手机：{{infos.phone}}</div>
+      <div class="person-email">邮箱：{{infos.email}}</div>
+      <div class="person-wechat">微信：{{infos.wechat}}</div>
+    </div>
+    <div class="bottom-line">
+      <div class="blue">{{infos.signOne}}</div>
+      <div class="red">{{infos.signTwo}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    infos: Object
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  margin-top: 30px;
+  background-color: #fff;
+  width: 350px;
+}
+.top-line {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 15px;
+}
+.top-line .blue {
+  width: 30%;
+  background-color: blue;
+  height: 15px;
+}
+.top-line .red {
+  width: 30%;
+  height: 15px;
+  background-color: red;
+}
+.company {
+  margin-left: 20px;
+  margin-top: 10px;
+  text-align: left;
+}
+.name {
+  font-size: 20px;
+  color: #244987;
+}
+.name-en {
+  font-size: 12px;
+  color: #244987;
+  margin-left: 2.5px;
+}
+.person {
+  margin-left: 40%;
+  width: 60%;
+  text-align: left;
+}
+.person-name .name {
+  font-size: 22px;
+  color: #000;
+}
+.person-name .work {
+  font-size: 12px;
+  color: #000;
+  margin-left: 8px;
+}
+.person-address {
+  margin-top: 8px;
+}
+.person-address,
+.person-phone,
+.person-email,
+.person-wechat {
+  font-size: 12px;
+}
+.bottom-line {
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 15px;
+}
+.bottom-line .blue {
+  font-size: 15px;
+  line-height: 30px;
+  text-align: center;
+  color: #fff;
+  width: 40%;
+  background-color: blue;
+  height: 30px;
+}
+.bottom-line .red {
+  font-size: 15px;
+  text-align: center;
+  color: #fff;
+  width: 60%;
+  background-color: red;
+  height: 30px;
+  line-height: 30px;
+}
+</style>
---
+++ src/views/pages/home/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="home">
+    <div class="list" v-for="(info,index) in infos" :key="index">
+      <Card :infos="info" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "home",
+  components: {
+    Card
+  },
+  created() {
+    this.loadData();
+  },
+  data() {
+    return {
+      infos: []
+    };
+  },
+  methods: {
+    loadData: function() {
+      this.infos = [
+        {
+          companyName: "某某科技",
+          companyNameEn: "MOUMOU TECH",
+          personName: "张三",
+          work: "java高级开发",
+          address: "浙江杭州滨江阿里附近",
+          phone: "159****9304",
+          email: "zhangsan@mmtech.com",
+          wechat: "zhangsan1234",
+          signOne: "会当凌绝顶",
+          signTwo: "一览众山小"
+        }
+      ];
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+.list {
+  display: flex;
+  justify-content: center;
+  background-color: #969696;
+  padding-bottom: 30px;
+}
+</style>
