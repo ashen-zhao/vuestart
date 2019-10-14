fbe1d2affe6a675b5c40954f97d7b81b29d91771
zhaoashen <zhaoashen@gmail.com>
2019-10-14 10:11:15 +0800

    完善百思

--- src/components/Duanzi.vue
+++ src/components/Duanzi.vue
@@ -22,5 +22,47 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+* {
+  background-color: #fff;
+}
+.container {
+  .header {
+    display: flex;
+    flex-direction: row;
+    margin-top: 10px;
+    .avator {
+      background-color: #f5f5f5;
+      margin-left: 10px;
+      width: 55px;
+      height: 55px;
+      border-radius: 10px;
+      text-align: center;
+      vertical-align: middle;
+      img {
+        margin-top: 1px;
+        width: 53px;
+        height: 53px;
+        border-radius: 10px;
+      }
+    }
+    .name-passtime {
+      font-size: 17px;
+      padding-left: 10px;
+      .name {
+        height: 50%;
+        text-align: left;
+      }
+      .passtime {
+        height: 50%;
+        text-align: left;
+      }
+    }
+  }
+  .content {
+    margin: 0px;
+    text-align: left;
+    padding: 10px;
+  }
+}
 </style>
