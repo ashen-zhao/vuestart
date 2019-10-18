ff17bdab5c2805efa4c13ad991371a6ed7c3e337
zhaoashen <zhaoashen@gmail.com>
2019-10-18 09:44:54 +0800

    为首页加入下拉加载更多

--- src/views/BaisiAll.vue
+++ src/views/BaisiAll.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
-    <pull-refresh v-model="isLoading" @refresh="onRefresh">
-      <div v-for="(item,index) in dataList" :key="index">
-        <b-video v-if="item.type == 'video'" :infos="item" />
-        <duanzi v-else-if="item.type == 'text'" :infos="item"/>
-        <pic v-else :infos="item"/>
-      </div>
+    <pull-refresh v-model="isFreshing" @refresh="onRefresh">
+      <list
+        v-model="isLoading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <cell v-for="(item,index) in dataList" :key="index">
+          <b-video v-if="item.type == 'video'" :infos="item" />
+          <duanzi v-else-if="item.type == 'text'" :infos="item" />
+          <pic v-else :infos="item" />
+        </cell>
+      </list>
     </pull-refresh>
   </div>
 </template>
@@ -14,7 +21,7 @@
 import Pic from "@/components/Picture.vue";
 import Duanzi from "@/components/Duanzi.vue";
 import BVideo from "@/components/BVideo.vue";
-import { PullRefresh } from "vant";
+import { List, Cell, PullRefresh } from "vant";
 
 export default {
   name: "BaisiAll",
@@ -22,12 +29,17 @@ export default {
     Pic,
     Duanzi,
     BVideo,
-    PullRefresh
+    PullRefresh,
+    List,
+    Cell
   },
   data() {
     return {
       dataList: [],
-      isLoading: false
+      isFreshing: false,
+      isLoading: true,
+      finished: false,
+      nextpage: "0"
     };
   },
   created() {
@@ -36,24 +48,48 @@ export default {
   methods: {
     onRefresh() {
       setTimeout(() => {
+        this.nextpage = "0";
+        this.finished = false;
+        this.isFreshing = true;
         this.getDataList();
       }, 500);
+    },
+    onLoad() {
+      if (!this.isFreshing) {
+        this.getDataList();
+      }
     },
     getDataList() {
       var that = this;
       this.$http
-        .get("/api/topic/list/jingxuan/1/baisi_xiaohao-iphone-4.1/0-150.json")
+        .get(
+          "/api/topic/list/jingxuan/1/baisi_xiaohao-iphone-4.1/" +
+            that.nextpage +
+            "-10.json"
+        )
         .then(function(resp) {
-          that.dataList = resp.data.list;
+          if (that.nextpage == "0") {
+            that.dataList = [];
+          }
+          that.dataList.push.apply(that.dataList, resp.data.list);
           that.isLoading = false;
+          that.isFreshing = false;
+          that.nextpage = resp.data.info.np;
+          //模拟数据总量
+          if (that.dataList.length >= 100) {
+            that.finished = true;
+          }
         })
-        .finally(function() {});
     }
   }
 };
 </script>
 <style scoped>
-.container{
-    background-color:#F5f5f5;
+.container {
+  background-color: #f5f5f5;
+}
+.van-cell {
+  margin: 0;
+  padding: 0;
 }
 </style>
